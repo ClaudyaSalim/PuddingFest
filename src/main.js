3b7dcd5c044cd6ddc3788a61d2fdf15a09113aef
ClaudyaSalim <claudya.salim@gmail.com>
2023-04-26 23:29:21 +0700

    upload final project

---
+++ src/main.js
@@ -0,0 +1,40 @@
+const data = {
+  name: "John Doe",
+  email: "johndoe@example.com"
+};
+
+function postData() {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+}
+
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+
+      }, false)
+    })
+
+    // firebase
+    // postData()
+  })()
